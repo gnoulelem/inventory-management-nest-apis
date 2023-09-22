8bff150c0eb1aa131e28d61e12b5184156dfeb57
Gnoulelem SIBABI <gnoulelem.sibabi@bennu.tech>
2023-09-22 19:30:23 +0000

    feat(core): added auth module to insider module

--- apps/store-gateway/src/insider/insider.module.ts
+++ apps/store-gateway/src/insider/insider.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
+import { AuthRepositoryModule } from '@store-apis/repositories/auth';
 import { InsiderService } from './service/insider.service';
 import { InsiderController } from './api/v1/controller/insider.controller';
 
 @Module({
-  imports: [HttpModule],
+  imports: [HttpModule, AuthRepositoryModule],
   providers: [InsiderService],
   controllers: [InsiderController],
 })
