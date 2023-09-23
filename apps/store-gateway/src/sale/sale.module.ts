270820b4a39b181447ce142a137ae55ded155f98
Gnoulelem SIBABI <gnoulelem.sibabi@bennu.tech>
2023-09-23 16:09:50 +0000

    feat(core): added sale publishing to aws topic

--- apps/store-gateway/src/sale/sale.module.ts
+++ apps/store-gateway/src/sale/sale.module.ts
@@ -1,12 +1,20 @@
 import { Module } from '@nestjs/common';
 import { SaleRepositoryModule } from '@store-apis/repositories/sale';
 import { AuthRepositoryModule } from '@store-apis/repositories/auth';
+import { SNSClient } from '@aws-sdk/client-sns';
 import { SaleService } from './service/sale.service';
 import { SaleController } from './api/v1/controller/sale.controller';
+import { ISaleAwsTopicProvider } from './provider/saleawstopic.provider';
 
 @Module({
   imports: [SaleRepositoryModule, AuthRepositoryModule],
-  providers: [SaleService],
+  providers: [
+    SaleService,
+    {
+      provide: ISaleAwsTopicProvider,
+      useFactory: () => new SNSClient({ region: 'us-east-1' }),
+    },
+  ],
   controllers: [SaleController],
 })
 export class SaleModule {}
--- apps/store-gateway/src/sale/service/sale.service.ts
+++ apps/store-gateway/src/sale/service/sale.service.ts
@@ -2,16 +2,40 @@ import { Injectable } from '@nestjs/common';
 import { ISaleRepository } from '@store-apis/repositories/sale';
 import { ISale, TCreateSale } from '@store-apis/domains/sale';
 import { InsertOneResult } from 'mongodb';
+import { ISaleAwsTopicProvider } from '../provider/saleawstopic.provider';
+import * as process from 'process';
+import { PublishCommand } from '@aws-sdk/client-sns';
 
 @Injectable()
 export class SaleService {
-  constructor(private readonly saleRepository: ISaleRepository) {}
+  constructor(
+    private readonly saleRepository: ISaleRepository,
+    private readonly salesAwsTopicProvider: ISaleAwsTopicProvider
+  ) {}
 
   async createSale(entityLike: TCreateSale): Promise<InsertOneResult<ISale>> {
     try {
-      return this.saleRepository.create(entityLike);
+      const [createResult] = await Promise.all([
+        this.saleRepository.create(entityLike),
+        this.publishSale(entityLike),
+      ]);
+      return createResult;
     } catch (error: unknown) {
       console.error('Error in creating a Sale', error);
+      throw error;
+    }
+  }
+
+  private async publishSale(entityLike: TCreateSale): Promise<void> {
+    try {
+      const input = {
+        TopicArn: process.env.SALE_AWS_TOPIC_ARN,
+        Message: JSON.stringify(entityLike),
+      };
+      const command = new PublishCommand(input);
+      await this.salesAwsTopicProvider.send(command);
+    } catch (error: unknown) {
+      console.error('Error in publishing a Sale', error);
       throw error;
     }
   }
